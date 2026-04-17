9b4b0ae8bee712e83c9376cd6faa7297d1ca04cd
dimkaboro <dmitrysag1606@gmail.com>
2026-04-17 20:51:06 +0200

    feat: configure Docker containerization for backend and frontend services with initial database schema migration

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 # 1. Адрес нашей базы данных
 # Мы берем эти данные (user, password, db) из твоего docker-compose.yml
 # Если ты ничего не менял в докере, то оставляй как есть.
-URL_DATABASE = "postgresql://user:password@127.0.0.1:5432/subscriptions_db"
+URL_DATABASE = os.getenv("DATABASE_URL", "postgresql://user:password@127.0.0.1:5432/subscriptions_db")
 
 # 2. Создаем "движок" (Engine)
 # Это сердце подключения. Через него запросы летят в базу.
